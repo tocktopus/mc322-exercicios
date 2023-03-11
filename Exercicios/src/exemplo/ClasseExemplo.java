de1e0cba133b02d056f5912313e935e4eb3d158b
Sara <sara.bsoliveira@gmail.com>
2023-03-10 21:29:10 -0300

    Update ClasseExemplo.java
    teste

--- Exercicios/src/exemplo/ClasseExemplo.java
+++ Exercicios/src/exemplo/ClasseExemplo.java
@@ -3,5 +3,6 @@ package exemplo;
 public class ClasseExemplo {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hoje é dia 10/03/2023");
 	}
 }
